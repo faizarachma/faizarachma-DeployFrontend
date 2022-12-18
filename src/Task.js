372b335aac2d8a9870838918a11cd95063c40ab8
faizarachma <faizarachma@gmail.com>
2022-12-18 21:49:07 +0700

    update assigment

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://tourmaline-entremet-64a869.netlify.app" // TODO: replace this
 const studentName = "Faiza Rachma" // TODO: replace this
 const studentId = "FE2878410" // TODO: replace this
 
